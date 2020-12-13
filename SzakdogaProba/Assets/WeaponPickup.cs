627bee6d65928118dfbba081cf6d56da189e1f6c
Gulyás Péter <gulyaspeter20@gmail.com>
2020-12-13 23:07:36 +0100

    1.

---
+++ SzakdogaProba/Assets/WeaponPickup.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class WeaponPickup : MonoBehaviour
+{
+    public Weapons weaponScript;
+    private System.Random rnd = new System.Random();
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        int weapon = rnd.Next(0, 2);
+        Debug.Log(other.tag);
+        if (other.tag == "Player")
+        {
+            switch (weapon)
+            {
+                case 0:
+                    weaponScript.Pistol = true;
+                    Debug.Log(weapon);
+                    Destroy(gameObject);
+                    break;
+                case 1:
+                    weaponScript.LaserGum = true;
+                    Destroy(gameObject);
+                    Debug.Log(weapon);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ SzakdogaProba/Assets/Weapons.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapons : MonoBehaviour
+{
+    public Transform firePoint;
+    public int damage = 40;
+    public GameObject impactEffect;
+    public LineRenderer lineRenderer;
+    public GameObject bulletPrefab;
+    public bool LaserGum = false;
+    public bool Pistol = false;
+    public GameObject pistol;
+    public GameObject laserGun;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (LaserGum)
+        {
+            laserGun.SetActive(true);
+            if (Input.GetButtonDown("Fire1"))
+            {
+                StartCoroutine(Shoot());
+                laserGun.SetActive(false);
+                LaserGum = false;
+            }
+        }
+        if (Pistol)
+        {
+            pistol.SetActive(true);
+            if (Input.GetButtonDown("Fire1"))
+            {
+                PistolShoot();
+                pistol.SetActive(false);
+                Pistol = false;
+            }
+        }
+    }
+
+    IEnumerator Shoot()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(firePoint.position, firePoint.right);
+
+        if (hitInfo)
+        {
+            Enemy enemy = hitInfo.transform.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+            lineRenderer.enabled = true;
+            lineRenderer.SetPosition(0, firePoint.position);
+            lineRenderer.SetPosition(1, hitInfo.point);
+        }
+        else
+        {
+            lineRenderer.SetPosition(0, firePoint.position);
+            lineRenderer.SetPosition(1, firePoint.position + firePoint.right * 100);
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        lineRenderer.enabled = false;
+    }
+
+    void PistolShoot()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+}
